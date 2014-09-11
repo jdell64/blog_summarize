cc08e841f1d507b7e63c31ac77fa1f8b062755cf
jdell64 <jeff.tindell@gmail.com>
2014-09-11 17:22:12 -0400

    updated

--- config_generator.py
+++ config_generator.py
@@ -13,13 +13,26 @@ config = ConfigParser.RawConfigParser()
 # mode. SafeConfigParser does not allow such assignments to take place.
 config.add_section('main')
 config.set('main', 'key', 'type mail chimp key here')
-config.set('main', 'template_id', 'type an int for the template id')
+# config.set('main', 'template_id', 'type an int for the template id')
 config.set('main', 'campaign_id', 'campaign_id_here')
 config.set('main', 'blogs', 'list,blogs,seperated,by,comma')
 # config.set('Section1', 'a_float', '3.1415')
 # config.set('Section1', 'baz', 'fun')
 # config.set('Section1', 'bar', 'Python')
 # config.set('Section1', 'foo', '%(bar)s is %(baz)s!')
+
+config.add_section('email')
+config.set('email', 'list_id', 'id for the list you are sending it to')
+config.set('email', 'subject', 'the subject of the email')
+config.set('email', 'from_email', 'who the email will appear to be from, eg Blogs@company.com')
+config.set('email', 'from_name', 'who the email will appear to be from, eg Company, INC')
+config.set('email', 'to_name', 'the name you are sending it to')
+
+# this is interval between emails in seconds
+# 60 sec * 60 min * 24 hr = 86400 seconds in a day
+# 86400 seconds * 7 days = 604800 seconds in a week
+config.set('email', 'wait_interval', 'seconds to wait inbetween emails')
+
 
 # Writing our configuration file to 'example.cfg'
 with open('myconfig.cfg', 'wb') as configfile:
--- updateTemplate.py
+++ updateTemplate.py
@@ -1,121 +1,158 @@
 import urllib2
 import mailchimp
 import os
+import logging
 import ConfigParser
 from bs4 import BeautifulSoup
+import time
+import sys
 from unidecode import unidecode
 
 
 # constants
 
 config = ConfigParser.RawConfigParser()
-
-
-#put in the loop
-
-config.read('myconfig.cfg')
-TEMPLATE_ID = config.getint('main', 'template_id')
-CAMPAIGN_ID = config.get('main','campaign_id')
-MAILCHIMP_API = config.get('main', 'key')
-
-
-
-# get blogs
-
-BLOG_LIST = config.get('main', 'blogs')
-
-BLOG_LIST = BLOG_LIST.split(",")
-
-blog_summaries = ["<div class='summaries'>"]
-for blog in BLOG_LIST:
-    # print blog
-    html =  urllib2.urlopen(blog).read()
-    urllib2.urlopen(blog).close()
-
-    soup = BeautifulSoup(html)
-
-    # print soup.h1['entry-title']
-    # print soup.h2['entry-title']
-    titles = soup.findAll("h2", { "class" : "entry-title" })
-    if len(titles) < 1:
-        titles = soup.findAll("h1", { "class" : "entry-title" })
-
-    most_recent_title = titles[0].string
-
-    contents = soup.findAll("div", { "class" : "entry-content" })
-    most_recent_content = contents[0].text
-    paragraphs = most_recent_content.split("\n")
-    paragraphs = filter(None, paragraphs) # drop empties
-
-    desired_text='<p>'+paragraphs[0]
-    index = 1
-    while len(desired_text.split(" ")) < 30: # if the desired text has less than 30 words
-        desired_text += '</p><p>' + paragraphs[index] # add the next paragraph
-        index += 1
-
-
-    blog_summary = '<div class="summary"><h2 class="title"><a href="'+ blog +'">'+most_recent_title+'</a></h1><div ' \
-                                                                               'class="content">'+desired_text+'</p></div></div>'
-    blog_summaries.append(blog_summary)
-
-blog_summaries.append('</div>')
-blog_summaries = "".join(blog_summaries)
-
-
-# for stupid blogs with stupid non-ASCII characters:
-
-blog_summaries = unidecode(blog_summaries)
-
-
-
-
-
-
-
-
-
-# will return the mailchimpAPI object
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def get_mailchimp_api():
     return mailchimp.Mailchimp(MAILCHIMP_API)  #your api key here
 
 
-m = get_mailchimp_api()
-
-template = m.templates.info(TEMPLATE_ID)
-# print template, type(template)
-source = template['source']
-# print source
-
-with open('head', 'r') as f:
-    head = f.read()
-with open('tail', 'r') as f:
-    tail = f.read()
 
 
-html = head+blog_summaries+tail
+# get blogs
+def get_blogs(blog_list):
+    logger.info("Started summarizing blogs: "+blog_list)
+    blog_list = blog_list.split(",")
+    blog_summaries = ["<div class='summaries'>"]
+    if len(blog_list) > 0:
+        for blog in blog_list:
+            logger.info("Getting blog: " + blog)
+            try:
+                html = urllib2.urlopen(blog).read()
+                urllib2.urlopen(blog).close()
+            except:
+                logger.error("Error with reading the website:" + blog)
+
+            soup = BeautifulSoup(html)
+            titles = soup.findAll("h2", {"class": "entry-title"})
+            if len(titles) < 1:
+                titles = soup.findAll("h1", {"class": "entry-title"})
+                if len(titles) < 1:
+                    logger.error("Unable to find any titles on blog:" + blog)
+                    break
+
+            most_recent_title = titles[0].string
+            contents = soup.findAll("div", {"class": "entry-content"})
+            most_recent_content = contents[0].text
+            paragraphs = most_recent_content.split("\n")
+            paragraphs = filter(None, paragraphs)  # drop empties
+            desired_text = '<p>' + paragraphs[0]
+            index = 1
+            while len(desired_text.split(" ")) < 30:  # if the desired text has less than 30 words
+                desired_text += '</p><p>' + paragraphs[index]  # add the next paragraph
+                logger.warn("Paragraph selection for "+blog+" was too short. Adding in next paragraph.")
+                index += 1
+            blog_summary = '<div class="summary"><h2 class="title"><a href="' + blog + '">' + most_recent_title + '</a></h1><div ' \
+                                                                                                                  'class="content">' + desired_text + '</p></div></div>'
+            blog_summaries.append(blog_summary)
+        logger.info("Finished with blog: "+blog)
+    else:
+        logger.error("No blogs in the blog list. Check the config file.")
+        return False
+    blog_summaries.append('</div>')
+    blog_summaries = "".join(blog_summaries)
+    # for stupid blogs with stupid non-ASCII characters:
+    blog_summaries = unidecode(blog_summaries)
+    logger.info("Finished summarizing blogs.")
+    return blog_summaries
 
 
-# print data
-# source = source.replace('REPLACE_TITLE', 'the real title')
-# source = source.replace('REPLACE_CONTENT', 'the real content')
+# will return the mailchimpAPI object
 
-new_value = {'html': html}
+def monkey_around(m, blog_summaries):
+    logger.info("Starting to monkey around.")
 
-# m.templates.update(TEMPLATE_ID, values=new_value)
 
-# create a new campaign
+    # template = m.templates.info(TEMPLATE_ID)
+    # print template, type(template)
+    # source = template['source']
+    # print source
 
-# put these in the config file:
-campaign_values = {'list_id':'6ff9a39094','subject':'CT BLOGS', 'from_email':'jeff.tindell@gmail.com','from_name':'jeff',
-                   'to_name':'Subscriber'}
+    with open('head', 'r') as f:
+        head = f.read()
+    with open('tail', 'r') as f:
+        tail = f.read()
 
-# print m.lists.list()
+    html = head + blog_summaries + tail
 
-camp = m.campaigns.create("regular", options=campaign_values, content=new_value)
-# camp['id']
+    # print data
+    # source = source.replace('REPLACE_TITLE', 'the real title')
+    # source = source.replace('REPLACE_CONTENT', 'the real content')
+
+    new_value = {'html': html}
+
+    # m.templates.update(TEMPLATE_ID, values=new_value)
+
+    # create a new campaign
+
+    # put these in the config file:
+    campaign_values = {'list_id': LIST_ID, 'subject': SUBJECT, 'from_email': FROM_EMAIL, 'from_name': FROM_NAME,
+                       'to_name': TO_NAME}
+
+    # print m.lists.list()
+    camp = m.campaigns.create("regular", options=campaign_values, content=new_value)
+
+    logger.info("Done monkeying around.")
+    return camp['id']
+    # camp['id']
+
+
 
 # SEND EMAIL
-m.campaigns.send(camp['id'])
+def send_mail(m, cid):
+    m.campaigns.send(cid)
 
 
+
+
+
+#put in the loop
+while (1):
+# read the config file every time you run
+
+    config.read('myconfig.cfg')
+    TEMPLATE_ID = config.getint('main', 'template_id')
+    CAMPAIGN_ID = config.get('main', 'campaign_id')
+    MAILCHIMP_API = config.get('main', 'key')
+    LIST_ID = config.get('email', 'list_id')
+    SUBJECT = config.get('email', 'subject')
+    FROM_EMAIL = config.get('email', 'from_email')
+    FROM_NAME = config.get('email', 'from_name')
+    TO_NAME = config.get('email', 'to_name')
+    BLOG_LIST = config.get('main', 'blogs')
+    SECONDS_TO_SLEEP = config.getint('email','wait_interval')
+    m = get_mailchimp_api()
+    blog_summaries=""
+    cid=0
+
+    try:
+        blog_summaries = get_blogs(BLOG_LIST)
+    except:
+        logger.error("Unexpected error getting blogs:" + sys.exc_info()[0])
+    #     SEND AN EMAIL HERE
+
+    try:
+        cid = monkey_around(m, blog_summaries)
+    except:
+        logger.error("Unexpected error monkeying around:" + sys.exc_info()[0])
+    # SEND AN EMAIL
+
+    try:
+        send_mail(m, cid)
+    except:
+        logger.error("Unexpected error sending email:"+ sys.exc_info()[0])
+    # SEND AN EMAIL
+
+    time.sleep(SECONDS_TO_SLEEP)
